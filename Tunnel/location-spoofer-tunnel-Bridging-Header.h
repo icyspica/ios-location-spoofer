63add33c4e444198b13c7142b0b93fb4c4373fa7
icyspica <nizaramra97@gmail.com>
2026-07-24 09:49:46 +0300

    Update location-spoofer-tunnel-Bridging-Header.h

--- Tunnel/location-spoofer-tunnel-Bridging-Header.h
+++ Tunnel/location-spoofer-tunnel-Bridging-Header.h
@@ -1,13 +1,10 @@
-//
-//  Bridging Header for Go Location Spoofer
-//  location-spoofer-tunnel
-//
-//  Import the CGo header for Swift access
-//
-
 #ifndef location_spoofer_tunnel_Bridging_Header_h
 #define location_spoofer_tunnel_Bridging_Header_h
 
 #include "golocationspoofer.h"
+
+// Fallback / Explicit C function declarations for Swift bridging:
+extern char* golocationspoofer_getcoords(void);
+extern char* golocationspoofer_drainlogs(void);
 
 #endif /* location_spoofer_tunnel_Bridging_Header_h */
